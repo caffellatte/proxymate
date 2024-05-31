b5563eb688262ee0c3e64423390cffd26dd3da16
Mikhail Lutsenko <m.g.lutsenko@gmail.com>
2024-05-31 23:46:43 +0300

    Install & Config `React`
    * Update `.eslintrc.json`: add _react/jsx-uses-react_ & _react/react-in-jsx-scope_ (.eslintrc.json);
    * Update `index.html`: update imported script & add _root_ div (index.html);
    * Install `react` & `react-dom` (package.json);
    * Move `src/index.css` to `src/ui/index.css`;
    * Remove `src/renderer.ts`;
    * Create `App.tsx`, move XState template from `src/renderer.ts` (src/ui/App.tsx);
    * Create `index.tsx`;
    * Update `tsconfig.json`.

---
+++ src/ui/App.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { actor } from "../machine";
+
+const App = () => {
+  actor.subscribe((snapshot) => {
+    console.log("Value:", snapshot.value);
+  });
+  actor.start();
+  actor.send({ type: "toggle" });
+  actor.send({ type: "toggle" });
+
+  return (
+    <>
+      <h1>Hello World!</h1>
+    </>
+  );
+};
+
+export default App;
