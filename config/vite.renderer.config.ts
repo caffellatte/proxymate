5318e495b9928d360e1adce0f624f1077299ae52
Mikhail Lutsenko <m.g.lutsenko@gmail.com>
2024-06-02 21:43:57 +0300

    Install `shadcn/ui` & `Inter` font
    * Add `import/resolver` for ESLint config (.eslintrc.json);
    * Create `shadcn/ui` cli config (components.json);
    * Add `vite-tsconfig-paths` plugin for vite renderer config (config/vite.renderer.config.ts);
    * Update `index.html`: add _Inter_ font & update renderer script path (index.html);
    * Install `eslint-import-resolver-typescript`, `vite-tsconfig-paths`, `@radix-ui/react-label`, `@radix-ui/react-slot`, `class-variance-authority`, `clsx`, `lucide-react`, `tailwind-merge` & `tailwindcss-animate` (package.json);
    * Update `postcss.config.js` export;
    * Rename `ui` fo `renderer` directory;
    * Rename `index.css` to `global.css`;
    * Add shadcn/ui `Button` (src/renderer/components/ui/button.tsx);
    * Update `tailwind.config.js`;
    * Update `tsconfig.json`;
    * Add `Inter` font (src/renderer/assets/fonts/inter/Inter-Black.ttf, src/renderer/assets/fonts/inter/Inter-Bold.ttf, src/renderer/assets/fonts/inter/Inter-ExtraBold.ttf, src/renderer/assets/fonts/inter/Inter-ExtraLight.ttf, src/renderer/assets/fonts/inter/Inter-Light.ttf, src/renderer/assets/fonts/inter/Inter-Medium.ttf, src/renderer/assets/fonts/inter/Inter-Regular.ttf, src/renderer/assets/fonts/inter/Inter-SemiBold.ttf, src/renderer/assets/fonts/inter/Inter-Thin.ttf).

--- config/vite.renderer.config.ts
+++ config/vite.renderer.config.ts
@@ -2,6 +2,7 @@ import type { ConfigEnv, UserConfig } from "vite";
 import { defineConfig } from "vite";
 import { pluginExposeRenderer } from "./vite.base.config";
 import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config
 export default defineConfig((env) => {
@@ -16,7 +17,7 @@ export default defineConfig((env) => {
     build: {
       outDir: `.vite/renderer/${name}`,
     },
-    plugins: [pluginExposeRenderer(name), react()],
+    plugins: [pluginExposeRenderer(name), react(), tsconfigPaths()],
     resolve: {
       preserveSymlinks: true,
     },
