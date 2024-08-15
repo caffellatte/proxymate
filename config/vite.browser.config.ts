3488413dc0604e876161a92d98ab953a76262ffb
Mikhail Lutsenko <m.g.lutsenko@gmail.com>
2024-08-15 23:42:04 +0300

    Update `vite.browser.config.ts`
    * Update `vite.browser.config.ts` config: change **root** & **build** (config/vite.browser.config.ts);
    * Create `index.html` for test **Browser** window (src/browser/index.html).

--- config/vite.browser.config.ts
+++ config/vite.browser.config.ts
@@ -3,19 +3,20 @@ import { defineConfig } from "vite";
 import { pluginExposeRenderer } from "./vite.base.config";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
+import path from "path";
 
 // https://vitejs.dev/config
 export default defineConfig((env) => {
-  const forgeEnv = env as ConfigEnv<"renderer">;
+  const forgeEnv = env as ConfigEnv<"browser">;
   const { root, mode, forgeConfigSelf } = forgeEnv;
   const name = forgeConfigSelf.name ?? "";
 
   return {
-    root,
+    root: path.join(root, "src", "browser"),
     mode,
     base: "./",
     build: {
-      outDir: `.vite/browser/${name}`,
+      outDir: path.join(root, `.vite/browser/${name}`),
     },
     plugins: [pluginExposeRenderer(name), react(), tsconfigPaths()],
     resolve: {
