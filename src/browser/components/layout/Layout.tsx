879940c860a6d2069b9a34ca243bf0efc412629e
Mikhail Lutsenko <m.g.lutsenko@gmail.com>
2024-08-19 17:50:25 +0300

    Create `TabsBar` component template
    * Create `TabsBar` component template (src/browser/components/templates/TabsBar/TabsBar.tsx, src/browser/components/templates/TabsBar/index.ts, src/browser/components/templates/index.ts);
    * Update `Layout`: add **TabsBar** (src/browser/components/layout/Layout.tsx);
    * Create `ITab` interface (src/interfaces/tab.ts).

--- src/browser/components/layout/Layout.tsx
+++ src/browser/components/layout/Layout.tsx
@@ -1,8 +1,9 @@
-import { SearchBar } from "@/browser/components/templates";
+import { SearchBar, TabsBar } from "@/browser/components/templates";
 
 const Layout = () => {
   return (
-    <div className="container py-5 flex flex-col gap-6">
+    <div className="container py-5 flex flex-col gap-4">
+      <TabsBar />
       <SearchBar />
     </div>
   );
